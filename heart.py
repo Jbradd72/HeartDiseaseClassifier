2a5e9009768899c83afdf2a35f95d5da9e14124d
Jeff <bra16024@byui.edu>
2019-11-25 23:00:01 -0700

    Added Random Forest Classifier

--- heart.py
+++ heart.py
@@ -10,7 +10,19 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
 from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 import pandas as pd
+
+def doRandomForest(iter):
+    print(iter)
+    rfc = RandomForestClassifier(n_estimators=iter, max_depth=2,
+                                  random_state=0)
+    rfc.fit(xTrain, yTrain)
+    predictions = rfc.predict(xTest)
+    score = accuracy_score(yTest, predictions)
+    return iter, score
 
 data = pd.read_csv('cardio_train.csv', sep=';')
 
@@ -34,11 +46,28 @@ for i in x['age']:
 x['age'] = new_list;
 xTrain, xTest, yTrain, yTest = train_test_split(x, y, train_size = .98, shuffle=True)
 
+best_score = 0;
+best_n = 0
+scores = []
+n = range(1,100)
+for i in n:
+    print(i)
+    rfc = RandomForestClassifier(n_estimators=i, max_depth=3,
+                                  random_state=0)
+    rfc.fit(xTrain, yTrain)
+    predictions = rfc.predict(xTest)
+    score = accuracy_score(yTest, predictions)
+    scores.append(score)
+    if score > best_score:
+        best_score = score
+        best_n = i
 
-classifier = KNeighborsClassifier(n_neighbors=numNeighbors)
-classifier.fit(xTrain, yTrain)
-predictions = classifier.predict(xTest)
 
-print("CAR: k = " + str(numNeighbors))
-print(confusion_matrix(yTest, predictions))
-print(classification_report(yTest, predictions))
+print(best_n, best_score)
+
+plt.plot(n, scores)
+plt.show()
+#print(confusion_matrix(yTest, predictions))
+#print(classification_report(yTest, predictions))
+
+#print("RFC accuracy: ", accuracy_score(yTest, predictions))
