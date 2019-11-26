5fc9dd9f356218ea2355c29cc34107836463e6e5
Jeff <bra16024@byui.edu>
2019-11-26 00:17:42 -0700

    Close to being done tbh

--- heart.py
+++ heart.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import pandas as pd
+from sklearn.naive_bayes import GaussianNB
 
 def doRandomForest(iter):
     print(iter)
@@ -47,26 +48,43 @@ x['age'] = new_list;
 xTrain, xTest, yTrain, yTest = train_test_split(x, y, train_size = .98, shuffle=True)
 
 best_score = 0;
-best_n = 0
+best_i = 0
+best_j = 1
+best_rfc = RandomForestClassifier(n_estimators=1, max_depth=1,
+                                      random_state=0)
 scores = []
 n = range(1,100)
 for i in n:
     print(i)
-    rfc = RandomForestClassifier(n_estimators=i, max_depth=3,
-                                  random_state=0)
-    rfc.fit(xTrain, yTrain)
-    predictions = rfc.predict(xTest)
-    score = accuracy_score(yTest, predictions)
-    scores.append(score)
-    if score > best_score:
-        best_score = score
-        best_n = i
+    for j in range(2,3):
+        rfc = RandomForestClassifier(n_estimators=i, max_depth=j,
+                                      random_state=0)
+        rfc.fit(xTrain, yTrain)
+        predictions = rfc.predict(xTest)
+        score = accuracy_score(yTest, predictions)
+        scores.append(score)
+        if score > best_score:
+            best_score = score
+            best_i = i
+            best_j = j
+            best_rfc = rfc
 
 
-print(best_n, best_score)
+print(best_i, best_j, best_score)
 
-plt.plot(n, scores)
-plt.show()
+
+final_pred = best_rfc.predict(xTest)
+ngb = GaussianNB()
+ngb.fit(xTrain, yTrain)
+
+xt = xTest.as_matrix()
+for i in range(len(final_pred)):
+    pred = final_pred[i]
+    if pred == 1:
+        print("you are going to die", ngb.predict_proba(xTest.to_numpy()[i].reshape(1,-1))[0][1])
+        
+    else:
+        print("Thanos has spared you", ngb.predict_proba(xTest.to_numpy()[i].reshape(1,-1))[0][0])
 #print(confusion_matrix(yTest, predictions))
 #print(classification_report(yTest, predictions))
 
